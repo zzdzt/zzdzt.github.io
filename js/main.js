91dae54c047b018074bc40ffe01eaea019757e0e
制杖的紫藤 <117342633+zzdzt@users.noreply.github.com>
2023-06-15 13:09:06 +0800

    Site updated: 2023-06-15 13:08:59

--- js/main.js
+++ js/main.js
@@ -825,3 +825,21 @@ document.addEventListener('DOMContentLoaded', function () {
   refreshFn()
   unRefreshFn()
 })
+
+var title;  // 用于临时存放原来的title内容
+window.onblur = function(){
+  // onblur时先存下原来的title,再更改title内容
+  title = document.title;
+  document.title = "快回来(ᗜ‸ᗜ)っ";
+};
+window.onfocus = function () {
+  // onfocus时原来的title不为空才替换回去
+  // 防止页面还没加载完成且onblur时title=undefined的情况
+  if(title) { 
+    document.title = "欢迎回来 ヾ(^▽^*)))";
+    titleTime = setTimeout(function() {
+      document.title = title;
+    }, 700);
+    // document.title = title;
+  }
+}
